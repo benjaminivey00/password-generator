aa3eec5072a955d0c750b6206200ed8404aef4ce
Ben Ivey <benjaminivey00@gmail.com>
2020-08-06 22:22:48 -0600

    added randomizing function

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,23 @@
+// generator functions
+function getRandomLower() {
+return String.fromCharCode(Math.floor(Math.random() *26) +97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() *26) +65);
+  }
+
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() *26) +48);
+    }
+
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*(){}+=<>/'
+     return symbols[Math.floor(Math.random() * symbols.length)];
+      }
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
