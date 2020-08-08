2a11528b2a07a91c581861782323aefb3ae68910
Ben Ivey <benjaminivey00@gmail.com>
2020-08-08 09:10:05 -0600

    got the radomizing functions working and first conditional

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,24 +1,4 @@
-// generator functions
-function getRandomLower() {
-return String.fromCharCode(Math.floor(Math.random() *26) +97);
-}
 
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() *26) +65);
-  }
-
-function getRandomNumber() {
-    return String.fromCharCode(Math.floor(Math.random() *26) +48);
-    }
-
-function getRandomSymbol() {
-  const symbols = '!@#$%^&*(){}+=<>/'
-     return symbols[Math.floor(Math.random() * symbols.length)];
-      }
-
-
-
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -32,3 +12,37 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+// generator functions
+
+let abclower =  function getRandomLower() {
+  const lowerCase = "abcdefghijklmnopqrstuvwxyz"
+  return lowerCase[Math.floor(Math.random() *lowerCase.length)];
+  
+}
+
+let abcupper =  function getRandomUpper() {
+    const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    return upperCase[Math.floor(Math.random() *upperCase.length)];
+    }
+  
+  
+  let number = function getRandomNumber() {
+    const number = "1234567890"
+      return number[Math.floor(Math.random() * number.length)] ;
+      }
+  
+  let symbol = function getRandomSymbol() {
+    const symbols = '!@#$%^&*(){}+=<>/'
+       return symbols[Math.floor(Math.random() * symbols.length)];
+        }
+        console.log(number());
+// user inputs
+ let passLength = prompt("Pick a password length between 8-128")
+        if(passLength <8 || passLength > 128){
+          alert("pick a differtent length")
+        }
+      else {
+        if (confirm("Do you want upper case letters in your password")){
+  
+}
+      }
